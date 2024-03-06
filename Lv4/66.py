041856df3e49c7ff6c522d46fa9facfa8b53388b
Juunsik <Juunsik29@gmail.com>
2024-03-06 09:56:32 +0900

    Add: 66. n의 배수 고르기

---
+++ Lv4/66.py
@@ -0,0 +1,11 @@
+# n의 배수 고르기
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/120905
+
+
+def solution(n, numlist):
+    answer = []
+    for i in numlist:
+        if i % n == 0:
+            answer.append(i)
+    return answer
