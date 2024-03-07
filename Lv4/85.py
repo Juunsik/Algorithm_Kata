c60eaa11fb52b53cd7e5a76d9e9f6f21d384dfa9
Juunsik <Juunsik29@gmail.com>
2024-03-07 10:03:01 +0900

    Add: 85. 배열의 원소만큼 추가하기

---
+++ Lv4/85.py
@@ -0,0 +1,11 @@
+# 배열의 원소만큼 추가하기
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/181861
+
+
+def solution(arr):
+    answer = []
+    for i in arr:
+        for _ in range(i):
+            answer.append(i)
+    return answer
