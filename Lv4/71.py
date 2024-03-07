95a02fda6b3021256bff9a60e75f5765cb4887af
Juunsik <Juunsik29@gmail.com>
2024-03-07 09:26:59 +0900

    Add: 71. 마지막 두 원소

---
+++ Lv4/71.py
@@ -0,0 +1,11 @@
+# 마지막 두 원소
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/181927
+
+
+def solution(num_list):
+    if num_list[-1] > num_list[-2]:
+        num_list.append(num_list[-1] - num_list[-2])
+    else:
+        num_list.append(num_list[-1] * 2)
+    return num_list
