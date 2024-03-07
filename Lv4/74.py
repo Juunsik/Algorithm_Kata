e09030acc550aad14296b2e5f39ec5832d478629
Juunsik <Juunsik29@gmail.com>
2024-03-07 09:35:43 +0900

    Add: 74. 가위 바위 보

---
+++ Lv4/74.py
@@ -0,0 +1,15 @@
+# 가위 바위 보
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/120839
+
+
+def solution(rsp):
+    answer = ""
+    for i in rsp:
+        if i == "2":
+            answer += "0"
+        if i == "0":
+            answer += "5"
+        if i == "5":
+            answer += "2"
+    return answer
