845cceda5068bcd389c060d7689c048c8a4394b0
Juunsik <Juunsik29@gmail.com>
2024-03-12 09:59:16 +0900

    Add: 118. 특별한 이차원 배열 1

---
+++ Lv5/118.py
@@ -0,0 +1,10 @@
+# 특별한 이차원 배열 1
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/181833
+
+
+def solution(n):
+    answer = [[0] * n for _ in range(n)]
+    for i in range(n):
+        answer[i][i] = 1
+    return answer
