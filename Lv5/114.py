e049a77b28cf3c4adf851420ae4bb1c6c6e795eb
Juunsik <Juunsik29@gmail.com>
2024-03-12 09:29:35 +0900

    Add: 114. 특별한 이차원 배열 2

---
+++ Lv5/114.py
@@ -0,0 +1,11 @@
+# 특별한 이차원 배열 2
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/181831
+
+
+def solution(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[i][j] != arr[j][i]:
+                return 0
+    return 1
