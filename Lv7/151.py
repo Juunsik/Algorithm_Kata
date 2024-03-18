e05518d18a8cd68717a9165d0c292b597dbb6aa3
Juunsik <Juunsik29@gmail.com>
2024-03-18 20:14:40 +0900

    Add: 151. 문자열 섞기

---
+++ Lv7/151.py
@@ -0,0 +1,10 @@
+# 문자열 섞기
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/181942
+
+
+def solution(str1, str2):
+    answer = ""
+    for i in range(len(str1)):
+        answer += str1[i] + str2[i]
+    return answer
