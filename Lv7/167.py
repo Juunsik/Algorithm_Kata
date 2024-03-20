d597d549211c4736d434daa6ca717bc0a66bf01d
Juunsik <Juunsik29@gmail.com>
2024-03-20 09:43:34 +0900

    Add: 167. 없는 숫자 더하기

---
+++ Lv7/167.py
@@ -0,0 +1,11 @@
+# 없는 숫자 더하기
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/86051
+
+
+def solution(numbers):
+    answer = 0
+    for i in range(10):
+        if i not in numbers:
+            answer += i
+    return answer
