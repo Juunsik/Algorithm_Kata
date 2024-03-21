2f4e5b87f5fc578a4eeef57d7678ab1fae4053d1
Juunsik <Juunsik29@gmail.com>
2024-03-21 09:40:57 +0900

    Add: 173. 문자열이 몇 번 등장하는지 세기

---
+++ Lv7/173.py
@@ -0,0 +1,11 @@
+# 문자열이 몇 번 등장하는지 세기
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/181871
+
+
+def solution(myString, pat):
+    answer = 0
+    for i in range(len(myString) - len(pat) + 1):
+        if pat == myString[i : i + len(pat)]:
+            answer += 1
+    return answer
