061730d175a6482c9c7cdfdf42dd8fdff8e8749d
Juunsik <Juunsik29@gmail.com>
2024-03-22 15:21:11 +0900

    Add: 183. 세 개의 구분자

---
+++ Lv8/183.py
@@ -0,0 +1,10 @@
+#  세 개의 구분자
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/181862
+
+
+def solution(myStr):
+    for i in ("a", "b", "c"):
+        myStr = myStr.replace(i, " ")
+    answer = myStr.split()
+    return answer if answer else ["EMPTY"]
