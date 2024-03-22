d0a05a92088f0bea06b0d1e13e862eae6682acb7
Juunsik <Juunsik29@gmail.com>
2024-03-22 15:55:35 +0900

    Add: 189. 수열과 구간 쿼리 4

---
+++ Lv8/189.py
@@ -0,0 +1,11 @@
+# 수열과 구간 쿼리 4
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/181922
+
+
+def solution(arr, queries):
+    for s, e, k in queries:
+        for i in range(s, e + 1):
+            if i == 0 or i % k == 0:
+                arr[i] += 1
+    return arr
