6f476ca7d6bb1ac6b1f3eeb8d040adcf4212840f
Juunsik <Juunsik29@gmail.com>
2024-03-26 09:45:04 +0900

    Add: 201. 소인수분해

---
+++ Lv8/201.py
@@ -0,0 +1,15 @@
+# 소인수분해
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/120852
+
+
+def solution(n):
+    answer = []
+    i = 2
+    while n > 1:
+        if n % i == 0:
+            answer.append(i)
+            n //= i
+        else:
+            i += 1
+    return sorted(list(set(answer)))
