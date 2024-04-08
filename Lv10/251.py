f495fc258512a8996e62ed0736d3d34f37eee2b7
Juunsik <Juunsik29@gmail.com>
2024-04-08 17:44:46 +0900

    fix: 재귀, 다중 반복문 사용하기

--- Lv10/251.py
+++ Lv10/251.py
@@ -1,3 +1,9 @@
+# 삼총사
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/131705
+
+
+# 재귀함수로 해결하기 
 def recur(stk, number, n):
     global answer
     if len(stk) == 3:
@@ -19,5 +25,16 @@ def solution(number):
 
     return answer
 
+#---------------------------------------------
 
-print(solution([-2, 3, 0, 2, -5]))
+# 삼중 반복문으로 해결하기
+def solution(number):
+    answer = 0
+
+    for i in range(len(number)):
+        for j in range(i+1,len(number)):
+            for k in range(j+1,len(number)):
+                if (number[i] + number[j] + number[k]) == 0:
+                    answer += 1
+
+    return answer
