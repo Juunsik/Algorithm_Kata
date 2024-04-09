84067932440e4d26234d0b18aad9ed8ca6b60194
Juunsik <Juunsik29@gmail.com>
2024-04-09 10:24:07 +0900

    Add: 263. 다항식 더하기

---
+++ Lv11/263.py
@@ -0,0 +1,19 @@
+# 다항식 더하기
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/120863
+
+
+def solution(polynomial):
+    xnum = 0
+    const = 0
+    for c in polynomial.split(" + "):
+        if c.isdigit():
+            const += int(c)
+        else:
+            xnum = xnum + 1 if c == "x" else xnum + int(c[:-1])
+    if xnum == 0:
+        return str(const)
+    elif xnum == 1:
+        return "x + " + str(const) if const != 0 else "x"
+    else:
+        return f"{xnum}x + {const}" if const != 0 else f"{xnum}x"
